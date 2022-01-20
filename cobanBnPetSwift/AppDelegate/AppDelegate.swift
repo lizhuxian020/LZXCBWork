60f58333452645ca85b8a7b5f0e1de0f15d4ee4e
lzx <lizhuxian@zzstc.cn>
2022-01-21 00:48:23 +0800

    用自己账号注册了百度Key,记得改回去=.=

--- cobanBnPetSwift/AppDelegate/AppDelegate.swift
+++ cobanBnPetSwift/AppDelegate/AppDelegate.swift
@@ -104,7 +104,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
         let baiduManager = BMKMapManager.init()
         /// "ez3XELufZYgRK5XGP70XSOGYkxjYZDdt" 宠物的
         /// "iubOgPuj5aLUwQF2mrRmFrXUXnbZM970" 物联网的
-        let result = baiduManager.start("iubOgPuj5aLUwQF2mrRmFrXUXnbZM970", generalDelegate: nil)
+//        let result = baiduManager.start("iubOgPuj5aLUwQF2mrRmFrXUXnbZM970", generalDelegate: nil)
+        let result = baiduManager.start("4AOKqVE1Ac5DtA8aD2IqkbtActwwSz8s", generalDelegate: nil)
+        
         if result {
             CBLog(message: "百度地图引擎启动成功")
         }
