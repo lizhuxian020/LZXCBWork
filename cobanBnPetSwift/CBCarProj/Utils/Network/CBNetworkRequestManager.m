db00abaeda2990cf1d751b8d2e1f8e05afd911c9
lzx <lizhuxian@zzstc.cn>
2022-11-22 22:50:48 +0800

    梳理网络逻辑, 不使用CBNetworkingServers, 因为没有做token过期处理

--- cobanBnPetSwift/CBCarProj/Utils/Network/CBNetworkRequestManager.m
+++ cobanBnPetSwift/CBCarProj/Utils/Network/CBNetworkRequestManager.m
@@ -35,54 +35,69 @@ const NSString *cbGetCallChargeParamters           = @"/personController/getDevi
                              success:(SuccessBlock)success
                              failure:(FailureBlock)failure {
     NSString *url = [NSString stringWithFormat:@"%@",bdtGetMyDeviceList];
-    [CBNetworkingServers networkRequestType:CBNetworkRequestTypeGet path:url param:paramters success:^(CBBaseNetworkModel * _Nonnull baseModel) {
-        !success?:success(baseModel);
-    } failure:^(NSError * _Nonnull error) {
-        !failure?:failure(error);
-    }];
+    [[NetWorkingManager shared] getWithUrl:url params:paramters succeed:^(id response, BOOL isSucceed) {
+        if (isSucceed) {
+            CBBaseNetworkModel *baseModel = [CBBaseNetworkModel mj_objectWithKeyValues:response];
+            !success?:success(baseModel);
+        }
+        } failed:^(NSError *error) {
+            !failure?:failure(error);
+        }];
 }
 /** 获取单个设备数据  */
 - (void)getSingeDeviceInfoDataParamters:(NSMutableDictionary *)paramters
                                 success:(SuccessBlock)success
                                 failure:(FailureBlock)failure {
     NSString *url = [NSString stringWithFormat:@"%@",bdtGetMySingeDeviceInfo];
-    [CBNetworkingServers networkRequestType:CBNetworkRequestTypeGet path:url param:paramters success:^(CBBaseNetworkModel * _Nonnull baseModel) {
-        !success?:success(baseModel);
-    } failure:^(NSError * _Nonnull error) {
-        !failure?:failure(error);
-    }];
+    [[NetWorkingManager shared] getWithUrl:url params:paramters succeed:^(id response, BOOL isSucceed) {
+        if (isSucceed) {
+            CBBaseNetworkModel *baseModel = [CBBaseNetworkModel mj_objectWithKeyValues:response];
+            !success?:success(baseModel);
+        }
+        } failed:^(NSError *error) {
+            !failure?:failure(error);
+        }];
 }
 /** 处理设备报警  */
 - (void)dealWithWarmedParamters:(NSMutableDictionary *)paramters
                         success:(SuccessBlock)success
                         failure:(FailureBlock)failure {
     NSString *url = [NSString stringWithFormat:@"%@",bdtDealWithDeviceWarmed];
-    [CBNetworkingServers networkRequestType:CBNetworkRequestTypeGet path:url param:paramters success:^(CBBaseNetworkModel * _Nonnull baseModel) {
-        !success?:success(baseModel);
-    } failure:^(NSError * _Nonnull error) {
-        !failure?:failure(error);
-    }];
+    [[NetWorkingManager shared] getWithUrl:url params:paramters succeed:^(id response, BOOL isSucceed) {
+        if (isSucceed) {
+            CBBaseNetworkModel *baseModel = [CBBaseNetworkModel mj_objectWithKeyValues:response];
+            !success?:success(baseModel);
+        }
+        } failed:^(NSError *error) {
+            !failure?:failure(error);
+        }];
 }
 /** 终端设备参数设置  */
 - (void)terminalSettingParamters:(NSMutableDictionary *)paramters
                          success:(SuccessBlock)success
                          failure:(FailureBlock)failure {
     NSString *url = [NSString stringWithFormat:@"%@",cbTerminalSettingParamters];
-    [CBNetworkingServers networkRequestType:CBNetworkRequestTypePost path:url param:paramters success:^(CBBaseNetworkModel * _Nonnull baseModel) {
-        !success?:success(baseModel);
-    } failure:^(NSError * _Nonnull error) {
-        !failure?:failure(error);
-    }];
+    [[NetWorkingManager shared] postWithUrl:url params:paramters succeed:^(id response, BOOL isSucceed) {
+        if (isSucceed) {
+            CBBaseNetworkModel *baseModel = [CBBaseNetworkModel mj_objectWithKeyValues:response];
+            !success?:success(baseModel);
+        }
+        } failed:^(NSError *error) {
+            !failure?:failure(error);
+        }];
 }
 /** 获取话费列表  */
 - (void)getCallChargeParamters:(NSMutableDictionary *)paramters
                        success:(SuccessBlock)success
                        failure:(FailureBlock)failure {
     NSString *url = [NSString stringWithFormat:@"%@",cbGetCallChargeParamters];
-    [CBNetworkingServers networkRequestType:CBNetworkRequestTypeGet path:url param:paramters success:^(CBBaseNetworkModel * _Nonnull baseModel) {
-        !success?:success(baseModel);
-    } failure:^(NSError * _Nonnull error) {
-        !failure?:failure(error);
-    }];
+    [[NetWorkingManager shared] getWithUrl:url params:paramters succeed:^(id response, BOOL isSucceed) {
+        if (isSucceed) {
+            CBBaseNetworkModel *baseModel = [CBBaseNetworkModel mj_objectWithKeyValues:response];
+            !success?:success(baseModel);
+        }
+        } failed:^(NSError *error) {
+            !failure?:failure(error);
+        }];
 }
 @end
--- cobanBnPetSwift/CBCarProj/Utils/Tools/NetWorkingManager.m
+++ cobanBnPetSwift/CBCarProj/Utils/Tools/NetWorkingManager.m
@@ -9,6 +9,7 @@
 #import "NetWorkingManager.h"
 /* oc 调用 swift*/
 #import "cobanBnPetSwift-Swift.h"
+#import "NSString+LBExtension.h"
 
 static NetWorkingManager *manager = nil;
 
@@ -23,8 +24,17 @@ static AFHTTPSessionManager *afManager = nil;
         afManager = [AFHTTPSessionManager manager];
         afManager.responseSerializer = [AFJSONResponseSerializer serializer];
         afManager.requestSerializer = [AFHTTPRequestSerializer serializer];
-        afManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/JavaScript",@"text/html",@"text/plain",@"image/jpeg",
-                                                               @"image/png", @"application/octet-stream",  nil];
+        afManager.responseSerializer.acceptableContentTypes =
+        [NSSet setWithObjects:
+         @"application/json",
+         @"text/json",
+         @"text/JavaScript",
+         @"text/html",
+         @"text/plain",
+         @"image/jpeg",
+         @"image/png",
+         @"application/x-www-form-urlencoded",
+         @"application/octet-stream",  nil];
     });
     return manager;
 }
@@ -58,7 +68,7 @@ static AFHTTPSessionManager *afManager = nil;
     CBPetLoginModel *userInfoModel = [CBPetLoginModelTool getUser];
     [afManager.requestSerializer setValue:userInfoModel.token?:@"" forHTTPHeaderField:@"token"];
     [afManager.requestSerializer setValue:[NSString stringWithFormat:@"%@",userInfoModel.uid] forHTTPHeaderField:@"uid"];
-    NSString *newUrl = [BASE_URL_CarNet stringByAppendingString:[NSString stringWithFormat:@"/%@",url]];
+    NSString *newUrl = [BASE_URL_CarNet stringByAppendingString:[NSString stringWithFormat:@"/%@",[url hasStringInPrefix:@"/"]]];
     NSLog(@"--------地址：%@",newUrl);
     NSLog(@"--------参数：%@",params);
     [afManager POST:newUrl parameters:params progress:^(NSProgress * _Nonnull uploadProgress) {
@@ -70,7 +80,9 @@ static AFHTTPSessionManager *afManager = nil;
             //NSLog(@"---------结果：%@",responseObject);
             if([responseObject[@"status"]integerValue] == 0) {//成功
                 succeed(responseObject,YES);
-            } else if([responseObject[@"status"]integerValue] == 105 || [responseObject[@"status"]integerValue] == 106 || [responseObject[@"status"]integerValue] == 107){
+            } else if([responseObject[@"status"]integerValue] == 105 ||
+                      [responseObject[@"status"]integerValue] == 106 ||
+                      [responseObject[@"status"]integerValue] == 107) {
                 succeed(responseObject,NO);
                 [HUD showHUDWithText: @"登 录 已 过 期\n请 重 新 登 录!" withDelay:1.2];
                 
@@ -88,6 +100,10 @@ static AFHTTPSessionManager *afManager = nil;
             } else if ([responseObject[@"status"]integerValue] == 1) {
                 succeed(responseObject,NO);
                 [HUD showHUDWithText:[Utils getSafeString:Localized(@"授权号码已存在")] withDelay:2.0];
+            } else if ([responseObject[@"status"]integerValue] == 205) { //从CBWtNetworkingServers移植过来的逻辑
+                succeed(responseObject,NO);
+                //状态205，代码联系人达到上限12个。。app这边有这样错误时提示“联系人已达上限”
+                [MBProgressHUD showMessage:Localized(@"联系人列表已满") withDelay:1.5];
             }
             else {
                 succeed(responseObject,NO);
@@ -110,9 +126,11 @@ static AFHTTPSessionManager *afManager = nil;
             }
         }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        BOOL isFailed = [self login:task];
         failed(error);
-        
-        [HUD showHUDWithText:Localized(@"请求超时") withDelay:1.2];
+        if (isFailed) {
+            [MBProgressHUD showMessage:Localized(@"请求超时") withDelay:1.5];
+        }
     }];
 }
 
@@ -134,7 +152,7 @@ static AFHTTPSessionManager *afManager = nil;
     CBPetLoginModel *userModel = [CBPetLoginModelTool getUser];
     [afManager.requestSerializer setValue:userModel.token?:@"" forHTTPHeaderField:@"token"];
     [afManager.requestSerializer setValue:[NSString stringWithFormat:@"%@",userModel.uid] forHTTPHeaderField:@"uid"];
-    NSString *newUrl = [BASE_URL_CarNet stringByAppendingString:[NSString stringWithFormat:@"/%@",url]];
+    NSString *newUrl = [BASE_URL_CarNet stringByAppendingString:[NSString stringWithFormat:@"%@",[url hasStringInPrefix:@"/"]]];
     NSLog(@"--------地址：%@",newUrl);
     NSLog(@"--------参数：%@",params);
     [afManager GET:newUrl parameters:params progress:^(NSProgress * _Nonnull downloadProgress) {
@@ -182,10 +200,41 @@ static AFHTTPSessionManager *afManager = nil;
             }
         }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        BOOL isFailed = [self login:task];
         failed(error);
-        
-        [HUD showHUDWithText:Localized(@"请求超时") withDelay:1.2];
+        if (isFailed) {
+            [MBProgressHUD showMessage:Localized(@"请求超时") withDelay:1.5];
+        }
     }];
+}
+
+//从CBWtNetworkingServers移植过来的逻辑
+- (BOOL)login:(NSURLSessionDataTask *)task {
+    NSHTTPURLResponse * responses = (NSHTTPURLResponse *)task.response;
+    if (responses.statusCode == 401) { // // 401 token 失效// 404 服务器连接失败，网络异常// 500 服务器出错
+        // 清除本地选中的设备token
+//        CBWtUserLoginModel *userModel = [CBWtUserLoginModel CBaccount];
+//        userModel.token = nil;
+//        [CBWtUserLoginModel saveCBAccount:userModel];
+        
+        CBPetLoginModel *userModel = [CBPetLoginModelTool getUser];
+        userModel.token = nil;
+        [CBPetLoginModelTool saveUser:userModel];
+        
+        UIAlertController *alertControl = [UIAlertController alertControllerWithTitle:nil message:Localized(@"用户信息异常，请重新登录") preferredStyle:UIAlertControllerStyleAlert];
+        // 知道了
+        [alertControl addAction:[UIAlertAction actionWithTitle:Localized(@"重新登录") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // 登录超时，请重新登录
+                [[NSNotificationCenter defaultCenter] postNotificationName:KCBWt_SwitchCBWtLoginViewController object:nil];
+            });
+        }]];
+        //[[CBWtCommonTools getCurrentVC] presentViewController:alertControl animated:YES completion:nil];
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alertControl animated:YES completion:nil];
+        return NO;
+    } else {
+        return YES;
+    }
 }
 
 @end
